7a7f97b66fa3853a21e3afab281eb07235033689
jquintus <josh.quintus@live.com>
2015-02-22 21:55:40 -0500

    initial commit

---
+++ ResumeMvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ResumeMvc.Startup))]
+namespace ResumeMvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
