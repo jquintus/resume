0606129f707bb45396d2b0ac46d68ecd1d67f9ce
jquintus <josh.quintus@live.com>
2015-02-22 22:32:03 -0500

    Remove all the default pages and login

---
+++ ResumeMvc/Models/Resume.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResumeMvc.Models
+{
+    public class Resume
+    {
+        public Resume()
+    {
+        Name = "Josh";
+        Title = "Senior Software Engineer";
+    }
+        public string Name { get; set; }
+        public string Title { get; set; }
+
+    }
+}
