8a70621ad15b4f85c1db12352af516a25a5e7cb3
jquintus <josh.quintus@live.com>
2015-02-23 11:12:55 -0500

    Code cleanup.  Nop

--- ResumeMvc/Models/Publication.cs
+++ ResumeMvc/Models/Publication.cs
@@ -1,16 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace ResumeMvc.Models
 {
-public    class Publication
+    public class Publication
     {
         public string Name { get; set; }
-        public string  Publisher { get; set; }
-        public string Website { get; set; }
+
+        public string Publisher { get; set; }
+
         public DateTime ReleaseDate { get; set; }
+
         public string Summary { get; set; }
+
+        public string Website { get; set; }
     }
 }
