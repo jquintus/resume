8966d2217bf80af8a22dd6df0f7da1ae87dcbf3b
jquintus <josh.quintus@live.com>
2015-02-23 21:45:49 -0500

    Add normalize.  Remove Bootstrap.

--- ResumeMvc/App_Start/BundleConfig.cs
+++ ResumeMvc/App_Start/BundleConfig.cs
@@ -8,6 +8,7 @@ namespace ResumeMvc
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            // Scripts
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
@@ -20,15 +21,18 @@ namespace ResumeMvc
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
+                      //"~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
 
             bundles.Add(new ScriptBundle("~/bundles/custom").Include(
                       "~/Scripts/custom/resizeDivs.js"));
+
+            // Styles
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      //"~/Content/bootstrap.css",
+                      "~/Content/normalize.css",
+                      "~/Content/site.css"));
+
 
             
 
